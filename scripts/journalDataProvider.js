210b5405a3f4eb69bf58fc7af0725fa6d68ca11e
Ni236630 <nicholas.douso@gmail.com>
2020-12-10 15:36:44 -0600

    refactoring entries to use json

--- scripts/journalDataProvider.js
+++ scripts/journalDataProvider.js
@@ -1,43 +1,26 @@
-/*
- *   Journal data provider for Daily Journal application
- *
- *      Holds the raw data about each entry and exports
- *      functions that other modules can use to filter
- *      the entries for different purposes.
- */
 
-// This is the original data.
-const journal = [
-  {
-      id: 1,
-      date: "07/24/2025",
-      concept: "HTML & CSS",
-      content: "We talked about HTML components and how to make grid layouts with Flexbox in CSS.",
-      mood: "Ok"
-    },
-    {
-      id: 2,
-      date: "07/26/2025",
-      concept: "HTML & CSS",
-      content: "We talked about more CSS indepth.",
-      mood: "Ok"
-    },
-    {
-      id: 3,
-      date: "07/27/2025",
-      concept: "Javascript",
-      content: "We discussed adding Javascript via <script> tag.",
-      mood: "Ok"
-  }
-]
+const journal = []
 
-/*
-  another function?
-*/
-export const useJournalEntries = () => {
-  const sortedByDate = journal.sort(
-      (currentEntry, nextEntry) =>
-          Date.parse(currentEntry.date) - Date.parse(nextEntry.date)
-  )
-  return sortedByDate
+export const getEntries = () => {
+  return fetch("http://localhost:8088/entries") // Fetch from the API
+      .then(response => response.json())  // Parse as JSON
+      .then(entries => {
+          journal = entries
+      })
 }
+
+
+
+export const useJournalEntries = () => {
+  return journal.slice()
+}
+
+
+
+// export const useJournalEntries = () => {
+//   const sortedByDate = journal.sort(
+//       (currentEntry, nextEntry) =>
+//           Date.parse(currentEntry.date) - Date.parse(nextEntry.date)
+//   )
+//   return sortedByDate
+// }
--- scripts/journalEntryList.js
+++ scripts/journalEntryList.js
@@ -1,19 +1,35 @@
-import { useJournalEntries } from "./journalDataProvider.js"
+import { getEntries,useJournalEntries } from "./journalDataProvider.js"
 import { journalEntryComponent } from "./journalEntry.js"
 
 
 
-export const entryListComponent = () => {
-  //DOM reference to where all entries will be rendered
-  const entryLog = document.querySelector("#entryLog")
-  //Use the journal entry data from the data provider component
-  const entries = useJournalEntries()
+// export const entryListComponent = () => {
+//   //DOM reference to where all entries will be rendered
+//   const entryLog = document.querySelector("#entryLog")
+//   //Use the journal entry data from the data provider component
+//   const entries = useJournalEntries()
   
   
-  for (const entry of entries){
-    const entryHTML = journalEntryComponent(entry)
+//   for (const entry of entries){
+//     const entryHTML = journalEntryComponent(entry)
     
-    entryLog.innerHTML += entryHTML
-  }
+//     entryLog.innerHTML += entryHTML
+//   }
   
+// }
+const contentTarget = document.querySelector("#entryLog")
+
+const render = (entryArray) => {
+  const allEntriesToStrings = entryArray.map((entry) => journalEntryComponent(entry)).join("")
+  
+  contentTarget.innerHTML = allEntriesToStrings
+}
+
+export const noteList = () => {
+  getEntries(
+    .then(()=> {
+      const allEntries = useJournalEntries()
+      render(allEntries)
+    })
+  )
 }
